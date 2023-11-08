1701e636e141fac8f3e0d08516c3c0c3d3843ffc
Cody DallaValle <cody.dallavalle@gmail.com>
2023-11-08 14:10:47 -0500

    Added block and transaction deserialization. Added chat VM

--- pwrgo/pwrgo.go
+++ pwrgo/pwrgo.go
@@ -9,9 +9,30 @@ import (
    "math/big"
 )
 
-type Response struct {
-   Data Data `json:"data"`
-   Status string `json:"status"`
+type Transaction struct {
+	PositionInTheBlock int `json:"positionInTheBlock"`
+	NonceOrValidationHash string `json:"nonceOrValidationHash"`
+	Size int `json:"size"`
+	Data string `json:"data"`
+	VmId int `json:"vmId"`
+	Fee int `json:"fee"`
+	From string `json:"from"`
+	To string `json:"to"`
+	TxnFee int `json:"txnFee"`
+	Type string `json:"type"`
+	Hash string `json:"hash"`
+}
+
+type Block struct {
+	BlockHash string `json:"blockHash"`
+	Success bool `json:"success"`
+	BlockNumber int `json:"blockNumber"`
+	BlockReward int `json:"blockReward"`
+	TransactionCount int `json:"transactionCount"`
+	Transactions   []Transaction `json:"transactions"`
+	BlockSubmitter string `json:"blockSubmitter"`
+	BlockSize int `json:"blockSize"`
+	Timestamp int `json:"timestamp"`
 }
 
 type Data struct {
@@ -19,6 +40,12 @@ type Data struct {
   Balance int `json:"balance,omitempty"`
   BlocksCount int `json:"blocksCount,omitempty"`
   ValidatorsCount int `json:"validatorsCount,omitempty"`
+  Block Block `json:"block,omitempty"`
+}
+
+type Response struct {
+   Data Data `json:"data"`
+   Status string `json:"status"`
 }
 
 func parseResponse(responseStr string) (response Response) {
@@ -53,10 +80,11 @@ func ValidatorsCount() (int) {
     return resp.Data.ValidatorsCount
 }
 
-func GetBlock(blockNumber int) (string) {
+func GetBlock(blockNumber int) (Block) {
     var blockNumberStr = strconv.Itoa(blockNumber)
     var response = get(RPC_ENDPOINT + "/block/?blockNumber=" + blockNumberStr)
-    return response
+	var resp = parseResponse(response)
+    return resp.Data.Block
 }
 
 func TransferPWR(to string, amount string, nonce int, privateKey *ecdsa.PrivateKey) (string) {
@@ -87,13 +115,13 @@ func TransferPWR(to string, amount string, nonce int, privateKey *ecdsa.PrivateK
     return result
 }
 
-func SendVMDataTx(vmIdStr string, data []byte, nonce int, privateKey *ecdsa.PrivateKey) (string) {
+func SendVMDataTx(vmId int64, data []byte, nonce int, privateKey *ecdsa.PrivateKey) (string) {
     if nonce < 0 {
         return "Nonce cannot be negative"
     }
 
-    vmId := new(big.Int)
-    vmId.SetString(vmIdStr, 10)
+    //vmId := new(big.Int)
+    //vmId.SetString(vmIdStr, 10)
 
     var buffer []byte
     buffer, err := vmDataBytes(vmId, nonce, data)
