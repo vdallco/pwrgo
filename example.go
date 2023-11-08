395c19738eede7ec2db6d45e6161a13ea51572ce
Cody DallaValle <cody.dallavalle@gmail.com>
2023-11-07 19:33:46 -0500

    Updated readme with SendVMDataTX() function signature and replaced tabs with spaces

--- example.go
+++ example.go
@@ -6,17 +6,17 @@ import (
 )
 
 func main() {
-	// Import wallet by private key
-	privateKeyHex := "0xE83385AF76B2B1997326B567461FB73DD9C27EAB9E1E86D26779F4650C5F2B75"
-	var wallet = pwrgo.FromPrivateKey(privateKeyHex)
-	
+    // Import wallet by private key
+    privateKeyHex := "0xE83385AF76B2B1997326B567461FB73DD9C27EAB9E1E86D26779F4650C5F2B75"
+    var wallet = pwrgo.FromPrivateKey(privateKeyHex)
+    
     fmt.Printf("Public key: %s\n", wallet.PublicKey)
     fmt.Printf("Address: %s\n", wallet.Address)
-	
-	// Get nonce for address
-	var nonce = pwrgo.NonceOfUser(wallet.Address)
-	fmt.Println("Nonce: ", nonce)
-
+    
+    // Get nonce for address
+    var nonce = pwrgo.NonceOfUser(wallet.Address)
+    fmt.Println("Nonce: ", nonce)
+    
     // Get PWR balance of address
     var balance = pwrgo.BalanceOf(wallet.Address)
     fmt.Println("Balance: ", balance)
@@ -24,28 +24,28 @@ func main() {
     // Get total blocks count
     var blocksCount = pwrgo.BlocksCount()
     fmt.Println("Blocks count: ", blocksCount)
-
-	// Get total validators count
+    
+    // Get total validators count
     var validatorsCount = pwrgo.ValidatorsCount()
     fmt.Println("Validators count: ", validatorsCount)
-
-	// Get block info by Block Number
-	var latestBlock = pwrgo.GetBlock(blocksCount - 1)
-	fmt.Println("Latest block: ", latestBlock)
-
+    
+    // Get block info by Block Number
+    var latestBlock = pwrgo.GetBlock(blocksCount - 1)
+    fmt.Println("Latest block: ", latestBlock)
+    
     // Transfer PWR
     var transferTx = pwrgo.TransferPWR(wallet.Address, "1", nonce, wallet.PrivateKey) // send 1 PWR to address, given nonce and private key bytes
     fmt.Println("Transfer tx : ", transferTx)
-
+    
     // Create new wallet and print address and keys
     var newWallet = pwrgo.NewWallet()
-	fmt.Println("New wallet address: ", newWallet.Address)
+    fmt.Println("New wallet address: ", newWallet.Address)
     fmt.Println("New wallet private key: ", newWallet.PrivateKeyStr)
     fmt.Println("New wallet public key: ", newWallet.PublicKey)
-
-	// Send data to VM 1337
-	var data = []byte("Hello world")
-	var dataTx = pwrgo.SendVMDataTx("1337", data, nonce, wallet.PrivateKey)
-	fmt.Println("Data VM tx: ", dataTx)
+    
+    // Send data to VM 1337
+    var data = []byte("Hello world")
+    var dataTx = pwrgo.SendVMDataTx("1337", data, nonce, wallet.PrivateKey)
+    fmt.Println("Data VM tx: ", dataTx)
 
 }
